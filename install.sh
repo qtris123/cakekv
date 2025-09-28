fd4f271e070ad79c599730eedb939650a7a8a819
tri vo <voquangtri2021@gmail.com>
2025-09-28 01:34:58 +0000

    change setting

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 pip install -e .
 
-sed -i '1161s/^[ \t]*logits = logits.float()/#&/' /opt/conda/lib/python3.10/site-packages/transformers/models/llama/modeling_llama.py
-sed -i '1044s/^[ \t]*logits = logits.float()/#&/' /opt/conda/lib/python3.10/site-packages/transformers/models/mistral/modeling_mistral.py
-sed -i '1069s/^[ \t]*logits = logits.float()/#&/' /opt/conda/lib/python3.10/site-packages/transformers/models/qwen2/modeling_qwen2.py
+sed -i '1161s/^[ \t]*logits = logits.float()/#&/' /usr/local/lib/python3.12/dist-packages/transformer/models/llama/modeling_llama.py
+sed -i '1044s/^[ \t]*logits = logits.float()/#&/' /usr/local/lib/python3.12/dist-packages/transformer/models/mistral/modeling_mistral.py
+sed -i '1069s/^[ \t]*logits = logits.float()/#&/' /usr/local/lib/python3.12/dist-packages/transformers/models/qwen2/modeling_qwen2.py
